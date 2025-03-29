ba11dde29eb62959641bd4d29cf51daa1eb556b3
Sċéon Morrissette <seon@eons.llc>
2025-03-28 22:49:12 -0700

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,15 +2,15 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'Eons Infrastructure Technologies',
+    description: 'The official source for all Eons workspace images.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://registry.workspace.infrastructure.tech/',
+    contactUrl: 'https://eons.llc/contact',
   },
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/kasm-registry/1.0',
+  basePath: '/kasm.registry/1.0',
   trailingSlash: true,
   images: {
     unoptimized: true,
